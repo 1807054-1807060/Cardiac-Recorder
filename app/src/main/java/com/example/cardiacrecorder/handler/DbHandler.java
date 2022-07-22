a6fb756d8dbac9ebfdd0ecf678150a5757fe097b
AnkanSaha18 <95054004+AnkanSaha18@users.noreply.github.com>
2022-07-22 23:02:50 +0600

    Updated unit test

--- app/src/main/java/com/example/cardiacrecorder/handler/DbHandler.java
+++ app/src/main/java/com/example/cardiacrecorder/handler/DbHandler.java
@@ -7,8 +7,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import androidx.annotation.Nullable;
-
 import com.example.cardiacrecorder.datetime.DateTime;
 import com.example.cardiacrecorder.model.CardiacRecord;
 import com.example.cardiacrecorder.params.Parameters;
@@ -24,23 +22,23 @@ public class DbHandler extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-       String query= "CREATE TABLE "+ Parameters.TABLE_NAME + " ( "
-               +Parameters.KEY_ID + " INTEGER PRIMARY KEY, "
-               +Parameters.KEY_NAME + " TEXT, "
-               +Parameters.KEY_DATE + " TEXT, "
-               +Parameters.KEY_TIME + " TEXT, "
-               +Parameters.KEY_SYS_PRESSURE + " INTEGER, "
-               +Parameters.KEY_DIA_PRESSURE + " INTEGER, "
-               +Parameters.KEY_HEART_RATE + " INTEGER, "
-               +Parameters.KEY_COMMENT + " TEXT" +")";
-               db.execSQL(query);
+        String query= "CREATE TABLE "+ Parameters.TABLE_NAME + " ( "
+                +Parameters.KEY_ID + " INTEGER PRIMARY KEY, "
+                +Parameters.KEY_NAME + " TEXT, "
+                +Parameters.KEY_DATE + " TEXT, "
+                +Parameters.KEY_TIME + " TEXT, "
+                +Parameters.KEY_SYS_PRESSURE + " INTEGER, "
+                +Parameters.KEY_DIA_PRESSURE + " INTEGER, "
+                +Parameters.KEY_HEART_RATE + " INTEGER, "
+                +Parameters.KEY_COMMENT + " TEXT" +")";
+        db.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
-    public void addRecord(CardiacRecord record){
+    public long addRecord(CardiacRecord record){
         SQLiteDatabase db= this.getWritableDatabase();
         ContentValues values= new ContentValues();
         values.put(Parameters.KEY_NAME, record.getName());
@@ -50,10 +48,25 @@ public class DbHandler extends SQLiteOpenHelper {
         values.put(Parameters.KEY_DIA_PRESSURE,record.getDiastolicPressure());
         values.put(Parameters.KEY_HEART_RATE,record.getHeartRate());
         values.put(Parameters.KEY_COMMENT,record.getComment());
-        db.insert(Parameters.TABLE_NAME, null, values);
+        long id = db.insert(Parameters.TABLE_NAME, null, values);
         Log.d("add-record", "addRecord: successfully added");
         db.close();
+        return id;
     }
+
+    public boolean checkIfDataExists(Long id) {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        String Query = "Select * from " + Parameters.TABLE_NAME + " where " + Parameters.KEY_ID + " = " + Long.toString(id);
+        Cursor cursor = sqLiteDatabase.rawQuery(Query, null);
+        if(cursor.getCount() <= 0){
+            cursor.close();
+            return false;
+        }
+        cursor.close();
+        return true;
+    }
+
+
     public List<CardiacRecord> showRecords(){
         List<CardiacRecord> records = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
@@ -91,11 +104,56 @@ public class DbHandler extends SQLiteOpenHelper {
         db.close();
     }
 
+    public boolean checkUpdated(CardiacRecord record, String newName, String newDate, String newTime, int newSystolicPressure, int newDiastolicPressure, int newHeartRate, String newComment)
+    {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query= "SELECT * FROM "+Parameters.TABLE_NAME + " where " + Parameters.KEY_ID + " = " +record.getId() ;
+        Cursor cursor= db.rawQuery(query,null);
+
+        if(cursor.moveToFirst()){
+            do{
+                String dbName = cursor.getString(1);
+                String dbDate = cursor.getString(1);
+                String dbTime = cursor.getString(2);
+                int dbSystolicPressure = Integer.parseInt(cursor.getString(4));
+                int dbDiastolicPressure = Integer.parseInt(cursor.getString(5));
+                int dbHeartRate = Integer.parseInt(cursor.getString(6));
+                String dbComment = cursor.getString(7);
+
+                if(dbName!=newName || dbDate!=newDate || dbTime!=newTime || dbSystolicPressure!=newSystolicPressure || dbDiastolicPressure!=newDiastolicPressure || dbHeartRate !=newHeartRate || dbComment!=newComment)
+                {
+                    cursor.close();
+                    return true;
+                }
+            }while(cursor.moveToNext());
+        }
+        db.close();
+        return false;
+
+
+
+
+
+
+
+
+//        SQLiteDatabase sqLiteDatabase =  this.getWritableDatabase();
+//        Cursor cursor = sqLiteDatabase.query(Parameters.TABLE_NAME, new String[]{Parameters.KEY_NAME}, Parameters.KEY_ID+" = '"+record.getId()+"'", null, null, null, null);
+//        while (cursor.moveToNext()) {
+//            int index1 = cursor.getColumnIndex(Parameters.KEY_NAME);
+//            String dbName = cursor.getString(index1);
+//            if(dbName != newName)
+//            {
+//                cursor.close();
+//                return false;
+//            }
+//        }
+//        return true;
+    }
+
     public void deleteRecord(CardiacRecord record){
         SQLiteDatabase db= this.getWritableDatabase();
         db.delete(Parameters.TABLE_NAME,Parameters.KEY_ID+"=?", new String[]{String.valueOf(record.getId())});
         db.close();
     }
-
-
 }
